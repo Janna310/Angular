4546c8f6e276ddbb0f17d0397ef1a60f05f6599d
Ankur <vasavada.ankur@gmail.com>
2020-04-03 16:52:34 -0400

    basic styling finished

--- Angular-Exercise2/src/app/components/circle/circle.component.ts
+++ Angular-Exercise2/src/app/components/circle/circle.component.ts
@@ -25,7 +25,6 @@ export class CircleComponent implements OnInit {
     return this.newSize;
   }
 
-  // ['border-bottom']: '3px solid lightgreen';
   toggleEvent() {
     this.isCircle = !this.isCircle;
   }
