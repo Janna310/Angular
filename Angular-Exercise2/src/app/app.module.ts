3ed51150abac7a90261b59a391463c92d2f2efd8
Ankur <vasavada.ankur@gmail.com>
2020-04-03 11:15:48 -0400

    created circle component

--- Angular-Exercise2/src/app/app.module.ts
+++ Angular-Exercise2/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { MatSliderModule } from '@angular/material/slider';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CircleComponent } from './components/circle/circle.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, CircleComponent],
   imports: [BrowserModule, BrowserAnimationsModule, MatSliderModule],
   providers: [],
   bootstrap: [AppComponent],
