0f38cc00f1e04fa7c91fccced1049f7f7ab4db33
Ankur <vasavada.ankur@gmail.com>
2020-04-03 10:48:17 -0400

    Installed Slider Module

--- Angular-Exercise2/src/app/app.module.ts
+++ Angular-Exercise2/src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { SliderModule } from '@syncfusion/ej2-angular-inputs';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, FormsModule, SliderModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
