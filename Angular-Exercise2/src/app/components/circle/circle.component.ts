1a3eeab53e8b16c06750bf50a1ed7ba492846fa4
Ankur <vasavada.ankur@gmail.com>
2020-04-03 16:36:23 -0400

    Got Circle to Square toggle working

--- Angular-Exercise2/src/app/components/circle/circle.component.ts
+++ Angular-Exercise2/src/app/components/circle/circle.component.ts
@@ -3,13 +3,26 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-circle',
   templateUrl: './circle.component.html',
-  styleUrls: ['./circle.component.css']
+  styleUrls: ['./circle.component.css'],
 })
 export class CircleComponent implements OnInit {
+  diameter = 1;
+  isCircle = true;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  changeDiameter(event) {
+    this.diameter = event.value;
   }
 
+  theSize = {
+    ['width']: this.diameter + 'px',
+    ['height']: this.diameter + 'px',
+  };
+
+  // ['border-bottom']: '3px solid lightgreen';
+  toggleEvent() {
+    this.isCircle = !this.isCircle;
+  }
 }
