ba441f1bf88c92c764143794b3303e2a4e651bf7
Ankur <vasavada.ankur@gmail.com>
2020-04-03 20:50:44 -0400

    Counter 2 & 3 done

--- Angular-Exercise3/src/app/components/up-down-counter2/up-down-counter2.component.ts
+++ Angular-Exercise3/src/app/components/up-down-counter2/up-down-counter2.component.ts
@@ -3,13 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-up-down-counter2',
   templateUrl: './up-down-counter2.component.html',
-  styleUrls: ['./up-down-counter2.component.css']
+  styleUrls: ['./up-down-counter2.component.css'],
 })
 export class UpDownCounter2Component implements OnInit {
+  count: number = 0;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
+  plusOne = () => this.count++;
 
+  minusOne = () => this.count--;
 }
--- Angular-Exercise3/src/app/components/up-down-counter3/up-down-counter3.component.ts
+++ Angular-Exercise3/src/app/components/up-down-counter3/up-down-counter3.component.ts
@@ -3,13 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-up-down-counter3',
   templateUrl: './up-down-counter3.component.html',
-  styleUrls: ['./up-down-counter3.component.css']
+  styleUrls: ['./up-down-counter3.component.css'],
 })
 export class UpDownCounter3Component implements OnInit {
+  count: number = 0;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
+  plusOne = () => this.count++;
 
+  minusOne = () => this.count--;
 }
